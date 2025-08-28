fecab2b62d8df1a746d644700dc9b883600f1df6
Mesith Rathnayake <mesithrathnayake0930@gmail.com>
2025-08-28 21:40:39 +0530

    ci: Add secret references to workflow

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/my-academia';
 require('dotenv').config();
 
 const app = express();
