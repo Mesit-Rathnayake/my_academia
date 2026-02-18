72f400cd90f985ca185d78d49a549a5ec6384b1b
Mesith Rathnayake <mesithrathnayake0930@gmail.com>
2026-02-18 22:52:05 +0530

    Fix App.test.js syntax error

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,4 +3,5 @@ import App from './App';
 
 test('renders my Academia app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/my Academia/i);
+  const linkElement = screen.getByText(/my Academia/i);  expect(linkElement).toBeInTheDocument();
+});
